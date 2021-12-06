09c9cedd4ffb8ccd78cf67099dadb8bb4153358f
tsili123 <atsilifonis@yahoo.com>
2021-12-06 22:19:06 +0200

    discrete

---
+++ algo/curves/discretef.hpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include <cstdlib>
+#include  <string>
+#include <cmath>
+#include <vector>
+#include <limits>
+using namespace std;
+
+double discreteFrechetDistance( vector<double> curve_query, vector<double> curve);
